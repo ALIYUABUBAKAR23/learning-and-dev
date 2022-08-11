e2c042f4d4076e5a95b186f1261ed4887674cb0d
Nasir Shehu <shehunasir7@gmail.com>
2022-08-11 22:43:59 +0100

    created a model for an item in the inventory

--- resources/admin.py
+++ resources/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from resources.models import Inventory
+from resources.models import Item
 
 # Register your models here.
 
 admin.site.register(Inventory)
+admin.site.register(Item)
