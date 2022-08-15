c2855e920302988dd716ebcc57de9d5afb00b823
Lexx Luey <71068965+LexxLuey@users.noreply.github.com>
2022-08-15 10:29:03 +0100

    added route for login and logout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/App.css";
-import {
-  HashRouter,
-  Route,
-  Switch,
-  Redirect,
-  BrowserRouter,
-} from "react-router-dom";
-import AuthLayout from "./layouts/auth";
-import AdminLayout from "./layouts/admin";
-import RTLLayout from "./layouts/rtl";
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme/theme";
 import App from "./App";
