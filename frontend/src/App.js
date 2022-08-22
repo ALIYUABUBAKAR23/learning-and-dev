e6ca8724f59dda98ff5622fc054a30ea5ec9abc5
Nasir Shehu <shehunasir7@gmail.com>
2022-08-22 09:57:49 +0100

    Created the UI for Resources and did unit tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import AdminLayout from "./layouts/admin";
 import RTLLayout from "./layouts/rtl";
 import SignIn from "./views/auth/signIn";
 import Cookies from "js-cookie";
+import ResourcesLayout from "./layouts/resources";
 
 function App() {
   function getToken() {
@@ -28,6 +29,7 @@ function App() {
         <Route path={`/auth`} component={AuthLayout} />
         <Route path={`/admin`} component={AdminLayout} />
         <Route path={`/rtl`} component={RTLLayout} />
+        <Route path={`/resources`} component={ResourcesLayout} />
         <Redirect from="/" to="/admin" />
       </Switch>
   );
