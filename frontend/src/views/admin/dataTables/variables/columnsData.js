7f8b3df7589cec3cc29e12bce81f2fda70dacbe8
hafseeman <hafoosah1@gmail.com>
2022-08-23 14:30:17 +0100

    created project table in datatable page

--- frontend/src/views/admin/dataTables/variables/columnsData.js
+++ frontend/src/views/admin/dataTables/variables/columnsData.js
@@ -1,3 +1,31 @@
+export const columnsDataProjects = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "DESCRIPTION",
+    accessor: "description",
+  },
+  {
+    Header: "PROJECT LEAD",
+    accessor: "project_lead",
+  },
+  {
+    Header: "START DATE",
+    accessor: "start_date",
+  },
+  {
+    Header: "END DATE",
+    accessor: "end_date",
+  },
+
+  {
+    Header: "ACTIONS",
+    accessor: "actions",
+  },
+];
+
 export const columnsDataDevelopment = [
   {
     Header: "NAME",
