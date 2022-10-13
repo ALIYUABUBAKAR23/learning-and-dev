2c57108a587506d793bea4a4cee4d4b469ec6c4c
hafseeman <hafoosah1@gmail.com>
2022-10-13 16:58:17 +0100

    removed comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,9 +30,9 @@ function App() {
   const loggedInUser = getUser();
 
   
-  // if (!token) {
-  //   return <SignIn />;
-  // }
+   if (!token) {
+     return <SignIn />;
+   }
 
   return (
       <UserContextProvider value={loggedInUser}> 
