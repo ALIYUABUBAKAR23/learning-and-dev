0d25b2cc590643abe4c45be6d1299ff542069aaf
abdulabdullah1000 <abdulabdullah1000@gmail.com>
2022-10-28 05:04:07 +0100

    reset uer details

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import UsersAPI
+from .views import UsersAPI, ProfileUpdateAPI
 
 urlpatterns = [
     path("", UsersAPI.as_view(), name="users"),
+    path("", ProfileUpdateAPI.as_view(), name="profile"),
+    
 ]
