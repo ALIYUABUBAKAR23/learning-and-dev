f937268652abadeaf734d4f11ccfc396e4669ea4
hafseeman <hafoosah1@gmail.com>
2022-11-14 12:37:47 +0100

    email verification edit and remove extra side routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -37,13 +37,6 @@ import SignInCentered from "./views/auth/signIn";
 
 const routes = [
   {
-    name: "Accounting",
-    layout: "/admin",
-    icon: <Icon as={MdGroupWork} width="20px" height="20px" color="inherit" />,
-    path: "/accounting",
-    component: Accounting,
-  },
-  {
     name: "Main Dashboard",
     layout: "/admin",
     path: "/default",
@@ -51,47 +44,11 @@ const routes = [
     component: MainDashboard,
   },
   {
-    name: "NFT Marketplace",
-    layout: "/admin",
-    path: "/nft-marketplace",
-    icon: (
-      <Icon
-        as={MdOutlineShoppingCart}
-        width="20px"
-        height="20px"
-        color="inherit"
-      />
-    ),
-    component: NFTMarketplace,
-    secondary: true,
-  },
-  {
-    name: "Data Tables",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
-    path: "/data-tables",
-    component: DataTables,
-  },
-  {
     name: "Profile",
     layout: "/admin",
     path: "/profile",
     icon: <Icon as={MdPerson} width="20px" height="20px" color="inherit" />,
     component: Profile,
-  },
-  {
-    name: "Sign In",
-    layout: "/auth",
-    path: "/sign-in",
-    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    component: SignInCentered,
-  },
-  {
-    name: "RTL Admin",
-    layout: "/rtl",
-    path: "/rtl-default",
-    icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-    component: RTL,
   },
   {
     name: "Tasks",
@@ -113,6 +70,13 @@ const routes = [
     path: "/items",
     icon: <Icon as={MdShoppingBasket} width="20px" height="20px" color="inherit" />,
     component: Item,
+  },
+  {
+    name: "Accounting",
+    layout: "/admin",
+    icon: <Icon as={MdGroupWork} width="20px" height="20px" color="inherit" />,
+    path: "/accounting",
+    component: Accounting,
   },
   {
     name: "Projects",
